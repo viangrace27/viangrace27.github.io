d50ee5a24742dffc74185b15257a54dac052892c
viangrace27.github.io <dud20001@byui.edu>
2020-10-13 18:23:53 +0800

    converting pounds to kilograms

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-/* Defining Table
- INPUT:  Input the number of pounds in box x
- PROCESSING: Convert pounds to kilograms and store in quotient
- OUTPUT: Display kilograms on the document
-*/ 
+/*Input:Enter a number in pounds to be converted to kilograms.
+	Processing: Convert pounds to kilograms
+	Round the distance to one digit after the decimal
+	Output: rounded kilograms*/
+	function convertpoundsTokilograms() {
+		//Input a number from the user.
+		let number1 = parseFloat(document.getElementById('number1').value);
 
-function poundsTokilogramsConverter() {
-	//INPUT
-	 let x = parseFloat(document.getElementById('numberofpounds').value);
-	 
-	//PROCESSING
-	 let quotient = ( x*1 ) / 2.20406;
-	//OUTPUT
-	 document.getElementById('output').innerHTML =  x + " pounds " + "is" + " equals " + qoutient + "kilograms" + "!";
-}
+		//Convert pounds to kilograms.
+		let kilograms = number1 / 2.20406;
+
+		//Round off answers in kilograms to one digit after the decimal
+		let digits = 1;
+		let multiplier = Math.pow(10, digits);
+		kilograms= Math.round(kilograms * multiplier) / multiplier;
+
+		//Display the kilograms to the user.
+	    document.getElementById('output').innerHTML =  number1 + " " + "Pounds"+ " " +"equals"+ " "+ kilograms +" " +"Kilograms";
+	}
