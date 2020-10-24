0cf9a341e661e60177a7f976f8115db9ee825beb
viangrace27.github.io <dud20001@byui.edu>
2020-10-24 21:30:01 +0800

    final output

--- calculator.js
+++ calculator.js
@@ -1,45 +1,35 @@
 function simpleCalculator() {
-  ///Input two numbers,operator and the user answer
-  
 
-  let x = parseFloat(document.getElementById('number1').value);
-  let y= parseFloat(document.getElementById('number2').value);
-  let answer = parseFloat(document.getElementById('number3').value);
-  let operator = document.getElementById('symbol').value;
-
-  //Solve the problem using switch operator
-  
-  
-  let answerKey;
-  
-  switch (operator) {
-
-      case '+':
-          answerKey = parseFloat(x + y);
-          break;
-
-      case '-':
-          answerKey = pareseFloat(x - y);
-          break;
-
-      case '*':
-          answerKey = parseFloat(x * y);
-          break;
-
-      case '/':
-          answerKey = parseFloat(x / y);
-          break;   
-  }
- 
-  //Output:
-  //User check if answer is correct.
-  let message;
-  if (answer == answerKey) {
-      message = "Your answer is Correct!.";
-  } 
-  else {
-      message = "Your answer is Incorrect.Try again.";
-  }
-  
-  document.getElementById('output').innerHTML = message;
+    //INPUT Input two numbers from the user and select operator from combo box.
+        
+    let number1 = parseFloat(document.getElementById('number1').value);
+    let operator = document.getElementById('operator').value;
+    let number2 = parseFloat(document.getElementById('number2').value);
+    let answer = parseFloat(document.getElementById('answer').value);
+    let answerKey;
+    let message;
+    
+    /*PROCESS: Use switch statement to solve basic arithmetic.
+      Use if block to check if  user's answer is correct or not */
+    switch (operator) {
+        case '+':
+            answerKey = number1 + number2;
+            break;
+        case '-':
+            answerKey = number1 - number2;
+            break;
+        case '*':
+            answerKey = number1 * number2;
+            break;
+        case '/':
+            answerKey = number1 / number2;
+    }
+    if (answer == answerKey) {
+        message = "Your answer is Correct!"
+    }
+    else {
+        message = "Your answer is Incorrect!"
+    }
+//OUTPUT: Message whether answer is correct or not.
+document.getElementById('output').innerHTML = message;
 }
