84c844d04630ce1feb162ed19e6cd27a2cb19881
viangrace27.github.io <dud20001@byui.edu>
2020-11-07 08:24:47 +0800

    adding comments

--- sumOdds.js
+++ sumOdds.js
@@ -10,7 +10,10 @@ function sumOddValue() {
      //INPUT: User input an interger n.
          
 
-//Identify odd numbers within interger n and odds them together.
+//Identify odd numbers within interger n and add them together.
+//USe for loop in adding odd numbers
+//Counting starts at 1 and the condition is i is equal to or less than the user input
+//Increment number by 2 since I am just adding the odd numbers
 let n = parseFloat(document.getElementById('number1').value);
 let sum = 0;
 for ( let i = 1; i <= n; i+=2) {
